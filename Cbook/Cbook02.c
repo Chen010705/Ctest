abf6b5051005de268b413fca19ad1af8faa42332
Chen010705 <909731145@qq.com>
2019-10-18 00:03:34 +0800

    0*45601650

---
+++ Cbook/Cbook02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+    int i,sum=0;
+    for(i=1;i<=100;i++)
+      if(i%2==0) sum=sum+i;
+    printf("%d",sum);
+
+    return 0;
+}
+/*教材C语言程序设计P014*/
+/*按照课本上码却无法运行？*/
