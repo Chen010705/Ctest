eaa27fe87465c9b567db24d5e4ae977b798556b2
Chen010705 <909731145.com>
2019-10-22 08:31:54 +0800

    11111

--- Hwork01.c
+++ Hwork01.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include<stdio.h>/*#include指令 包含头文件<stdio.h>*/
 #include<math.h>
-int main(void)
+int main(void)/*main函数 void表示空*/
 {
-    int radius,r3;
-    double PI,volume;
+    int radius,r3;/*int整型 声明radius，r3变量*/
+    double PI,volume;/*double双精度浮点型 声明PI，volume变量*/
     
     scanf("%d",&radius);
     PI =3.141592;
@@ -11,5 +11,5 @@ int main(void)
     volume = 4/3.0 * PI * r3;
     printf("Volume is %f!\n",volume);
 
-    return 0;
+    return 0;/*return语句*/
 }
