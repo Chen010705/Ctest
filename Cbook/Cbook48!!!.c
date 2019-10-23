aad60f4a47b14ad9cdeb0944c1bc03b495ec7915
Chen010705 <909731145@qq.com>
2019-10-23 20:51:02 +0800

    1023 20.51

---
+++ Cbook/Cbook48!!!.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+    char ch;
+
+    printf("input characters:");
+    ch=getchar();
+
+    while(ch!='\n'){
+        if(ch>='A'&&ch<='Z')
+        ch=ch-'A'+'a';
+        else if(ch>='a'&&ch<='z')
+        ch=ch-'a'&&ch<='z';
+        putchar (ch);
+        ch=getchar();
+    }
+    return 0;
+}/*P143*/
--- Cbook/tempCodeRunnerFile.c
+++
@@ -1,2 +0,0 @@
-
-    printf("product of complex is % f+% fi \n", res
