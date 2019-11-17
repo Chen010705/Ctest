84900cebf97ff3211b78367afdf84f806ae71281
Chen010705 <909731145@qq.com>
2019-11-17 10:26:14 +0800

    11.17 10.26

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int sum = 0;
+    int i;
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+        if (i == 3)
+            goto LOOP;
+    }
+LOOP:
+    printf("½áÊøforÑ­»·ÁË....");
+    return 0;
+}
---
+++ test/Ctest14.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+    char c;
+
+    for(c='Z';c>='A';c--)
+    {
+        printf("%c ",c);
+    }
+    return 0;
+}
