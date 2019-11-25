ac977ce68475201ea2ed355bf933ee510cf812df
Chen010705 <909731145@qq.com>
2019-11-25 21:32:32 +0800

    11.25

---
+++ Hwork/Hwork10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int i, j, k;
+    for (i = 0; i <= 100; i++)
+        for (j = 0; j <= 100; j++)
+            for (k = 0; k <= 100; k++)
+            {
+                if (5 * i + 3 * j + k / 3 == 100 && k % 3 == 0 && i + j + k == 100)
+                {
+                    printf("%d,%d,%d\n", i, j, k);
+                }
+            }
+    return 0;
+}
