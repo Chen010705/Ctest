ecd9b60e8ec2227b11b19a89e4d10f0b463bab0d
Chen010705 <909731145@qq.com>
2019-11-26 08:40:24 +0800

    156046

--- Hwork/Hwork10.c
+++ Hwork/Hwork10.c
@@ -2,8 +2,8 @@
 int main(void)
 {
     int i, j, k;
-    for (i = 0; i <= 100; i++)
-        for (j = 0; j <= 100; j++)
+    for (i = 0; i <= 20; i++)
+        for (j = 0; j <= 33; j++)
             for (k = 0; k <= 100; k++)
             {
                 if (5 * i + 3 * j + k / 3 == 100 && k % 3 == 0 && i + j + k == 100)
